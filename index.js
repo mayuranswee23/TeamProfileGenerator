12bce2e1d262e962be46f64cccf53c50ee213c36
Mayuran <mayuranswee23@gmail.com>
2021-07-04 14:41:48 -0400

    added inquirer prompts to employees

---
+++ index.js
@@ -0,0 +1,14 @@
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const inquirer = require('inquirer');
+const jest = require('jest');
+const fs = require('fs');
+
+const team = () => {
+    return inquirer.prompt([
+        
+    ])
+}
