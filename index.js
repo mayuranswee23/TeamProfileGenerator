06b74659e61da9631532c2d654f1d6efe55243a6
Mayuran <mayuranswee23@gmail.com>
2021-07-05 12:29:07 -0400

    added inquirer prompts for employees

--- index.js
+++ index.js
@@ -7,8 +7,97 @@ const inquirer = require('inquirer');
 const jest = require('jest');
 const fs = require('fs');
 
-const team = () => {
+teamMembers = [];
+
+function team() {
+    function getManager(){
     return inquirer.prompt([
-        
+        {
+            type: 'input',
+            message: 'Enter the name of the team manager',
+            name: 'managerName'
+        },
+        {
+            type: 'input',
+            message: 'Enter the employee ID of the manager',
+            name: 'managerID'
+        },
+        {
+            type: 'input',
+            message: "Enter the manager's email",
+            name: 'managerEmail'
+        },
+        {
+            type: 'input',
+            message: "Enter the manager's office number",
+            name: 'managerOfficeNumber'
+        },
+        {
+            type: 'list',
+            message: 'Which employee would you like to input next?',
+            name: 'employeeSelect', 
+            choices: ['Engineer', 'Intern', 'Exit']
+    },
     ])
+    .then(response => {
+        const manager = new Manager (response.managerName, reponse.managerID, reponse.managerEmail, reponse.managerOfficeNumber);
+        teamMembers.push(manager);
+    })
+}
+    function getEngineer (){
+        return inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Enter the name of the engineer',
+                name: 'engineerName'
+            },
+            {
+                type: 'input',
+                message: 'Enter the employee ID of the engineer',
+                name: 'engineerID'
+            },
+            {
+                type: 'input',
+                message: "Enter the engineer's email",
+                name: 'engineerEmail'
+            },
+            {
+                type: 'input',
+                message: "Enter the engineer's github username",
+                name: 'engineerGithub'
+            }
+        ])
+        .then(response => {
+            const engineer = new Engineer (response.engineerName, reponse.engineerID, reponse.engineerEmail, reponse.engineerGithub);
+            teamMembers.push(engineer);
+        })
+    }
+    function getIntern (){
+        return inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Enter the name of the intern',
+                name: 'internName'
+            },
+            {
+                type: 'input',
+                message: 'Enter the employee ID of the intern',
+                name: 'internID'
+            },
+            {
+                type: 'input',
+                message: "Enter the intern's email",
+                name: 'internEmail'
+            },
+            {
+                type: 'input',
+                message: "Enter the intern's school name",
+                name: 'internSchool'
+            }
+        ])
+        .then(response => {
+            const intern = new Engineer (response.engineerName, reponse.engineerID, reponse.engineerEmail, reponse.engineerGithub);
+            teamMembers.push(engineer);
+        })
+    }
 }
